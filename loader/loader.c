074ed6892732215017400d8c61ca6e5d9bbe6a37
Rishabh4Jakhar <rishabhjakhar04@gmail.com>
2024-08-30 13:19:42 +0530

    Copied launcher.c main to loader.c

--- loader/loader.c
+++ loader/loader.c
@@ -113,3 +113,29 @@ else {
 }
 close(fd); // Closing file descriptor
 }
+
+int main(int argc, char** argv) 
+{
+  // Wrong Usage of Command
+  if(argc != 2) {
+    printf("Usage: %s <ELF Executable> \n",argv[0]);
+    exit(1);
+  }
+  // 1. carry out necessary checks on the input ELF file
+  
+  FILE *elf = fopen(argv[1], "rb");
+  // Opening ELF file from argv[1] (path to ELF file)
+  if (!elf) // If file did not open
+  {
+    printf("Error: ELF File didn't open\n");
+    exit(1);
+  }
+  // Closing the file
+  fclose(elf);
+  // 2. passing it to the loader for carrying out the loading/execution
+  load_and_run_elf(argv[1]);
+  // 3. invoke the cleanup routine inside the loader  
+  loader_cleanup();
+  return 0;
+}
+ 
